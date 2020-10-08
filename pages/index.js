5b554eee62ce52c69885067de3050c21649c82b3
Febriansyah <febrian@efner.id>
2020-10-08 12:59:21 +0700

    add feedback in login page

--- pages/index.js
+++ pages/index.js
@@ -3,25 +3,57 @@ import Router from "next/router";
 
 import Box from "@material-ui/core/Box";
 import Container from "@material-ui/core/Container";
+import Snackbar from "@material-ui/core/Snackbar";
+import MuiAlert from "@material-ui/lab/Alert";
 
 import Copyright from "../components/Copyright/Copyright";
 import LoginForm from "../components/Forms/auth/LoginForm";
-import {setToken} from '../utils/localStorage';
+import { setToken } from "../utils/localStorage";
+import { useState } from "react";
+
+function Alert(props) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
 
 export default function Home() {
+  const [error, setError] = useState({
+    isError: false,
+    message: "",
+  });
 
   function loginHandler(username, password) {
-    if(username === 'admin') {
-      if(password === 'admin') {
+    if (username === "admin") {
+      if (password === "admin") {
         const user = {
           name: "Admin",
-          token: btoa("admin")
-        }
-        setToken(JSON.stringify(user))
-        Router.push('admin/dashboard')
+          token: btoa("admin"),
+        };
+        setToken(JSON.stringify(user));
+        Router.push("admin/dashboard");
+      } else {
+        setError({
+          isError: true,
+          message: "Password Salah",
+        });
       }
+    } else {
+      setError({
+        isError: true,
+        message: "Username Tidak Dikenal",
+      });
     }
   }
+
+  const handleClose = (_, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setError({
+      isError: false,
+      message: "",
+    });
+  };
 
   return (
     <Container component="main" maxWidth="xs">
@@ -37,6 +69,15 @@ export default function Home() {
       <Box mt={8}>
         <Copyright />
       </Box>
+      <Snackbar
+        open={error.isError}
+        autoHideDuration={2000}
+        onClose={handleClose}
+      >
+        <Alert onClose={handleClose} severity="warning">
+          {error.message}
+        </Alert>
+      </Snackbar>
     </Container>
   );
 }
